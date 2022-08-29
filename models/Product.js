ca4899124a563daf4a1f442566db0d36c865e6f8
LorenzoF <lorenzoferritsic@gmail.com>
2022-08-29 19:10:39 -0400

    added product model column and references

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allownull: false,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      types: DataTypes.INTEGER,
+      allownull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "category",
+        key: "id",
+      }
+    }
   },
   {
     sequelize,
